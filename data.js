62f42b39287bd1755c005dbcb23d4470e3ff1834
illussioon <153229496+illussioon@users.noreply.github.com>
2024-11-16 23:11:00 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,22 @@
+[
+  {
+    "id": 100000,
+    "title_ru": "Название",
+    "title_en": "Title",
+    "description": "Описание аниме",
+    "data_drop": 2022,
+    "anime_genres": ["Экшен", "Комедия"],
+    "poster_original": "https://example.com/poster.jpg",
+    "player_url": "https://example.com/player"
+  },
+  {
+    "id": 100002,
+    "title_ru": "Название",
+    "title_en": "Title",
+    "description": "Описание аниме",
+    "data_drop": 2022,
+    "anime_genres": ["Экшен", "Комедия"],
+    "poster_original": "https://example.com/poster.jpg",
+    "player_url": "https://example.com/player"
+  }
+]
