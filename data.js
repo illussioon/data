348b38dd8515883dd002bac15ccfd624e0d79df5
illussioon <153229496+illussioon@users.noreply.github.com>
2024-11-17 00:17:48 +0200

    Update data.js

--- data.js
+++ data.js
@@ -19,13 +19,13 @@
     "poster_original": "https://example.com/poster.jpg",
     "player_url": "https://example.com/player",
     "videos": [
-     {
+      {
         "episode": 1,
         "fhd": null,
         "hd": "https://raw.githubusercontent.com/illussioon/view/main/d8d783b9/playlist_14.m3u8",
         "sd": null,
         "alternative_player_url": null
-    }
+      }
     ]
   },
   {
@@ -38,12 +38,13 @@
     "poster_original": "https://example.com/poster.jpg",
     "player_url": "https://example.com/player",
     "videos": [
-     {
+      {
         "episode": 1,
         "fhd": null,
         "hd": "https://raw.githubusercontent.com/illussioon/view/main/d8d783b9/playlist_14.m3u8",
         "sd": null,
         "alternative_player_url": null
-     }
-]
+      }
+    ]
+  }
 ]
