edd4bc147a7dce4a21ed68e099b5ac85d9c99c60
illussioon <153229496+illussioon@users.noreply.github.com>
2024-11-16 23:11:52 +0200

    Update data.js

--- data.js
+++ data.js
@@ -17,6 +17,7 @@
     "data_drop": 2022,
     "anime_genres": ["Экшен", "Комедия"],
     "poster_original": "https://example.com/poster.jpg",
-    "player_url": "https://example.com/player"
+    "player_url": "https://example.com/player",
+    "videos" : []
   }
 ]
