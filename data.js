58a0316ebfbe457abf4f6505370f31630da81439
illussioon <153229496+illussioon@users.noreply.github.com>
2024-11-16 23:51:36 +0200

    Update data.js

--- data.js
+++ data.js
@@ -25,6 +25,23 @@
         "hd": "https://raw.githubusercontent.com/illussioon/view/main/d8d783b9/playlist_14.m3u8",
         "sd": null,
         "alternative_player_url": null
+    },
+      {
+    "id": 222223,
+    "title_ru": "Тетра",
+    "title_en": "Title",
+    "description": "Описание аниме",
+    "data_drop": 2022,
+    "anime_genres": ["Экшен", "Комедия"],
+    "poster_original": "https://example.com/poster.jpg",
+    "player_url": "https://example.com/player",
+    "videos": [
+     {
+        "episode": 1,
+        "fhd": null,
+        "hd": "https://raw.githubusercontent.com/illussioon/view/main/d8d783b9/playlist_14.m3u8",
+        "sd": null,
+        "alternative_player_url": null
     }
     ]
   }
