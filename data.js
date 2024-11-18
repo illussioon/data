901b70fa411607556683cbd51903810162256175
illussioon <153229496+illussioon@users.noreply.github.com>
2024-11-19 00:46:34 +0200

    Update data.js

--- data.js
+++ data.js
@@ -22,7 +22,7 @@
       {
         "episode": 1,
         "fhd": null,
-        "hd": "https://raw.githubusercontent.com/illussioon/view/main/705c69d9/new.m3u8",
+        "hd": "https://raw.githubusercontent.com/illussioon/view/main/dde19cd5/episode24/playlist_24.m3u8",
         "sd": null,
         "alternative_player_url": null
       }
