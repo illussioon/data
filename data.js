03110eca97dade35dece3c797c6fb458f2141b62
illussioon <153229496+illussioon@users.noreply.github.com>
2024-11-22 10:28:12 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,15 +1,5 @@
 [
   {
-    "id": 100000,
-    "title_ru": "Название",
-    "title_en": "Title",
-    "description": "Описание аниме",
-    "data_drop": 2022,
-    "anime_genres": ["Экшен", "Комедия"],
-    "poster_original": "https://example.com/poster.jpg",
-    "player_url": "https://example.com/player"
-  },
-  {
     "id": 222221,
     "title_ru": "Тетра",
     "title_en": "Title",
@@ -23,25 +13,6 @@
         "episode": 1,
         "fhd": null,
         "hd": "https://raw.githubusercontent.com/illussioon/view/main/dde19cd5/episode24/playlist_24.m3u8",
-        "sd": null,
-        "alternative_player_url": null
-      }
-    ]
-  },
-  {
-    "id": 222223,
-    "title_ru": "Тетра",
-    "title_en": "Title",
-    "description": "Описание аниме",
-    "data_drop": 2022,
-    "anime_genres": ["Экшен", "Комедия"],
-    "poster_original": "https://example.com/poster.jpg",
-    "player_url": "https://example.com/player",
-    "videos": [
-      {
-        "episode": 1,
-        "fhd": null,
-        "hd": "https://raw.githubusercontent.com/illussioon/view/main/d8d783b9/playlist_14.m3u8",
         "sd": null,
         "alternative_player_url": null
       }
