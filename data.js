173d4eb7f4eb423396731fcaf2da3a018562bca1
illussioon <153229496+illussioon@users.noreply.github.com>
2024-11-22 10:34:22 +0200

    Update data.js

--- data.js
+++ data.js
@@ -3,16 +3,16 @@
     "id": 222221,
     "title_ru": "Тетра",
     "title_en": "Title",
-    "description": "Описание аниме",
+    "description": "Лайт Ягами — образцовый 17-летний выпускник, баллы за экзамены которого находятся в первых строках рейтинга всей Японии. Сидя на уроке, он замечает, что за окном что-то упало. На перемене он поднимает загадочный предмет и им оказывается черная тетрадь с надписью «Тетрадь смерти». Внутри была инструкция по использованию: "Человек, имя которого будет записано в тетради, умрет". Имея свои взгляды на систему наказания, Лайт решает установить собственное правосудие, использовать тетрадь для «очищения» мира от зла — убивать преступников.",
     "data_drop": 2022,
-    "anime_genres": ["Экшен", "Комедия"],
-    "poster_original": "https://example.com/poster.jpg",
+    "anime_genres": ["Сёнен", "Сверхъестественное, Триллер"],
+    "poster_original": "https://raw.githubusercontent.com/illussioon/data/main/photo_2024-11-22_10-29-24.jpg",
     "player_url": "https://example.com/player",
     "videos": [
       {
         "episode": 1,
         "fhd": null,
-        "hd": "https://raw.githubusercontent.com/illussioon/view/main/dde19cd5/episode24/playlist_24.m3u8",
+        "hd": "https://download1479.mediafire.com/zqitxu60axigfRgdDaU8xkB4iNaHJPbc0B6FUl9VlL7meWpY5aUE3f-ABkOBVs3cZHRzstGbvAw9OO_hJQA_U2s-Wp02deNvJxHXA9T8oK-iJWetwp0qvJ5dc3Zlt0AwKAtLheicGyxi0o8QJNKC7kDz7KzEFw38b7dDj6xtHw-Z6g/2j512fzwik486ce/episode1.mp4",
         "sd": null,
         "alternative_player_url": null
       }
