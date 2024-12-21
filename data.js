7ddd4f41659fe1dc2c35d85035a0ccfbd810d4b0
kompromizz <153229496+illussioon@users.noreply.github.com>
2024-12-21 15:23:52 +0200

    Update data.js

--- data.js
+++ data.js
@@ -12,259 +12,259 @@
       {
         "episode": 1,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode1.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode1.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 2,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode2.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode2.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 3,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode3.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode3.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 4,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode4.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode4.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 5,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode5.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode5.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 6,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode6.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode6.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 7,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode7.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode7.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 8,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode8.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode8.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 9,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode9.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode9.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 10,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode10.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode10.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 11,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode11.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode11.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 12,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode12.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode12.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 13,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode13.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode13.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 14,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode14.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode14.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 15,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode15.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode15.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 16,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode16.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode16.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 17,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode17.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode17.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 18,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode18.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode18.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 19,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode19.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode19.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 20,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode20.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode20.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 21,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode21.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode21.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 22,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode22.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode22.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 23,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode23.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode23.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 24,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode24.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode24.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 25,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode25.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode25.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 26,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode26.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode26.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 27,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode27.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode27.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 28,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode28.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode28.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 29,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode29.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode29.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 30,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode30.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode30.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 31,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode31.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode31.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 32,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode32.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode32.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 33,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode33.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode33.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 34,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode34.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode34.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 35,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode35.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode35.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 36,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode36.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode36.mp4",
         "sd": null,
         "alternative_player_url": null
       },
       {
         "episode": 37,
         "fhd": null,
-        "hd": "https://cdn.illussion.fun/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode37.mp4",
+        "hd": "https://cdn.illussion.art/videos/X9wHErn5eoRNclZaIJSufoiZZRmZS2/episode37.mp4",
         "sd": null,
         "alternative_player_url": null
       }
